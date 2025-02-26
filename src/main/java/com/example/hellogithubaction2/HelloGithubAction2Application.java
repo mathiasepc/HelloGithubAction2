d20c55d86e396179942c242efa9875175e2d17b6
mathiasepc <mathiasepc@msn.com>
2025-02-26 12:33:29 +0100

    initial commit

---
+++ src/main/java/com/example/hellogithubaction2/HelloGithubAction2Application.java
@@ -0,0 +1,13 @@
+package com.example.hellogithubaction2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGithubAction2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloGithubAction2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellogithubaction2/HelloGithubAction2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellogithubaction2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGithubAction2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
